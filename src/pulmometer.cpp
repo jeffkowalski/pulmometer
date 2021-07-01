679e9c50cc957eb34281615f6256fcc1c6f07949
Jeff Kowalski <jeff.kowalski@gmail.com>
2021-07-01 16:13:26 -0700

    add calibration on initialization

--- src/pulmometer.cpp
+++ src/pulmometer.cpp
@@ -30,31 +30,56 @@ static void record_to_database (int counter) {
 }
 
 
-#define MAX_MAGNITUDE 8860
-#define MIN_MAGNITUDE 7310
-#define HYSTERESIS    0.3  // [0.0 .. 0.5)
-#define THRESH_LOW    (long)(MIN_MAGNITUDE + HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE))
-#define THRESH_HIGH   (long)(MAX_MAGNITUDE - HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE))
-static bool zero_crossing (long val, bool & state) {
+static Adafruit_LIS3MDL lis3mdl;
+
+
+static int read_sensor_mag() {
+    lis3mdl.read();  // get X Y and Z data at once
+    int mag = (int)sqrt ((double)lis3mdl.x * (double)lis3mdl.x + (double)lis3mdl.y * (double)lis3mdl.y +
+                           (double)lis3mdl.z * (double)lis3mdl.z);
+#ifdef DEBUG
+    Serial.printf ("%6d = |(%5d, %5d, %5d)|\n", mag, lis3mdl.x, lis3mdl.y, lis3mdl.z);
+#endif
+    return mag;
+}
+
+
+// configurable options to adjust sensitivity
+static long  CALIBRATION_DURATION = 1000 * 60 * 5; // five minutes
+static float HYSTERESIS = 0.3;  // [0.0 .. 0.5)
+
+// set initial values, to be overwritten in calibration
+static int   MAX_MAGNITUDE = INT_MIN;
+static int   MIN_MAGNITUDE = INT_MAX;
+static int   THRESH_LOW = 0;
+static int   THRESH_HIGH = 0;
+
+static void calibrate (long duration) {
+    long stop = millis() + duration;
+    Serial.printf ("Calibrating...\n");
+    Serial.printf ("%7s %7s %7s %7s\n", "time", "curr", "min", "max");
+    Serial.printf ("%7s %7s %7s %7s\n", "-------", "-------", "-------", "-------");
+    long now;
+    while ((now = millis()) < stop) {
+        int mag = read_sensor_mag();
+        if (mag < MIN_MAGNITUDE)
+            MIN_MAGNITUDE = mag;
+        if (mag > MAX_MAGNITUDE)
+            MAX_MAGNITUDE = mag;
+        Serial.printf ("\r%7ld %7d %7d %7d", stop-now, mag, MIN_MAGNITUDE, MAX_MAGNITUDE);
+    }
+    THRESH_LOW  = (int)(MIN_MAGNITUDE + HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE));
+    THRESH_HIGH = (int)(MAX_MAGNITUDE - HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE));
+    Serial.printf ("\nDone.  MIN = %d, MAX = %d\n", MIN_MAGNITUDE, MAX_MAGNITUDE);
+}
+
+
+static bool zero_crossing (int val, bool & state) {
     if ((state && val < THRESH_LOW) || (!state && val > THRESH_HIGH)) {
         state = !state;
         return true;
     } else
         return false;
-}
-
-
-static Adafruit_LIS3MDL lis3mdl;
-
-
-static long read_sensor_mag() {
-    lis3mdl.read();  // get X Y and Z data at once
-    long mag = (long)sqrt ((double)lis3mdl.x * (double)lis3mdl.x + (double)lis3mdl.y * (double)lis3mdl.y +
-                           (double)lis3mdl.z * (double)lis3mdl.z);
-#ifdef DEBUG
-    Serial.printf ("%6ld = |(%5d, %5d, %5d)|\n", mag, lis3mdl.x, lis3mdl.y, lis3mdl.z);
-#endif
-    return mag;
 }
 
 
@@ -66,7 +91,7 @@ static void sensor_task (void * parameter) {
     bool state     = false;  // arbitrary initial value
     bool state_set = false;  // tracks "arbitraryness" of state
     while (true) {
-        long mag = read_sensor_mag();
+        int mag = read_sensor_mag();
         if (zero_crossing (mag, state) &&  // modifies state, returns true if changed
             state && state_set) {
             Serial.println ("breath");
@@ -123,6 +148,8 @@ void setup() {
                              true,   // polarity
                              false,  // don't latch
                              true);  // enabled!
+
+    calibrate (CALIBRATION_DURATION);
 
     xMutex = xSemaphoreCreateMutex();
     xTaskCreate (sensor_task, "Sensor Task", 4096, NULL, 1, NULL);
