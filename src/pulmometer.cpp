d45b7358ec7004a7a170392607b5f2a2e5547b95
Jeff Kowalski <jeff.kowalski@gmail.com>
2024-07-13 19:08:31 -0700

    add ota updates, threshold reporting

--- src/pulmometer.cpp
+++ src/pulmometer.cpp
@@ -1,10 +1,12 @@
+#include "credentials.h"
 #include <Adafruit_LIS3MDL.h>
+#include <ElegantOTA.h>
 #include <HTTPClient.h>
 #include <WiFi.h>
-#include "credentials.h"
 
+#define SERVER        "192.168.7.207:8086"  // address of influx database
+#define WIFI_HOSTNAME "pulmometer"
 
-#define SERVER "192.168.7.207:8086"  // address of influx database
 static void record_to_database (int counter) {
     WiFiClient client;
     HTTPClient http;
@@ -29,30 +31,27 @@ static void record_to_database (int counter) {
     http.end();
 }
 
-
 static Adafruit_LIS3MDL lis3mdl;
 
-
-static int read_sensor_mag() {
+static int read_sensor_mag () {
     lis3mdl.read();  // get X Y and Z data at once
     int mag = (int)sqrt ((double)lis3mdl.x * (double)lis3mdl.x + (double)lis3mdl.y * (double)lis3mdl.y +
-                           (double)lis3mdl.z * (double)lis3mdl.z);
+                         (double)lis3mdl.z * (double)lis3mdl.z);
 #ifdef DEBUG
     Serial.printf ("%6d = |(%5d, %5d, %5d)|\n", mag, lis3mdl.x, lis3mdl.y, lis3mdl.z);
 #endif
     return mag;
 }
 
-
 // configurable options to adjust sensitivity
-static long  CALIBRATION_DURATION = 1000 * 60 * 5; // five minutes
-static float HYSTERESIS = 0.3;  // [0.0 .. 0.5)
+static long  CALIBRATION_DURATION = 1000 * 60 * 5;  // five minutes
+static float HYSTERESIS           = 0.3;            // [0.0 .. 0.5)
 
 // set initial values, to be overwritten in calibration
-static int   MAX_MAGNITUDE = INT_MIN;
-static int   MIN_MAGNITUDE = INT_MAX;
-static int   THRESH_LOW = 0;
-static int   THRESH_HIGH = 0;
+static int MAX_MAGNITUDE = INT_MIN;
+static int MIN_MAGNITUDE = INT_MAX;
+static int THRESH_LOW    = 0;
+static int THRESH_HIGH   = 0;
 
 static void calibrate (long duration) {
     long stop = millis() + duration;
@@ -66,26 +65,24 @@ static void calibrate (long duration) {
             MIN_MAGNITUDE = mag;
         if (mag > MAX_MAGNITUDE)
             MAX_MAGNITUDE = mag;
-        Serial.printf ("\r%7ld %7d %7d %7d", stop-now, mag, MIN_MAGNITUDE, MAX_MAGNITUDE);
+        Serial.printf ("\r%7ld %7d %7d %7d", stop - now, mag, MIN_MAGNITUDE, MAX_MAGNITUDE);
     }
     THRESH_LOW  = (int)(MIN_MAGNITUDE + HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE));
     THRESH_HIGH = (int)(MAX_MAGNITUDE - HYSTERESIS * (MAX_MAGNITUDE - MIN_MAGNITUDE));
     Serial.printf ("\nDone.  MIN = %d, MAX = %d\n", MIN_MAGNITUDE, MAX_MAGNITUDE);
 }
 
-
 static bool zero_crossing (int val, bool & state) {
     if ((state && val < THRESH_LOW) || (!state && val > THRESH_HIGH)) {
         state = !state;
         return true;
-    } else
+    }
+    else
         return false;
 }
 
-
 static SemaphoreHandle_t xMutex;
 volatile static int      counter = 0;
-
 
 static void sensor_task (void * parameter) {
     bool state     = false;  // arbitrary initial value
@@ -106,7 +103,6 @@ static void sensor_task (void * parameter) {
     }
 }
 
-
 static void recorder_task (void * parameter) {
     while (true) {
         if (WiFi.status() != WL_CONNECTED)
@@ -125,8 +121,9 @@ static void recorder_task (void * parameter) {
     }
 }
 
+ELEGANTOTA_WEBSERVER server (80);
 
-void setup() {
+void setup () {
     Serial.begin (112500);
     delay (1000);  // Safety
 
@@ -134,6 +131,32 @@ void setup() {
     WiFi.begin (WIFI_SSID, WIFI_PSK);  // defined in credentials.h
     WiFi.waitForConnectResult();       // so much neater than those stupid loops and dots
     Serial.println (WiFi.localIP());
+
+#if ELEGANTOTA_USE_ASYNC_WEBSERVER == 1
+    #define REQUEST_ARG AsyncWebServerRequest * request
+    #define DO_SEND     request->send
+#else
+    #define REQUEST_ARG
+    #define DO_SEND server.send
+#endif
+    server.on ("/", [] (REQUEST_ARG) {
+        String html = "";
+        html += "<html>";
+        html += WIFI_HOSTNAME;
+        html += "<br/>MIN = " + String (MIN_MAGNITUDE) + "; MAX = " + String (MAX_MAGNITUDE) +
+                "; THRESH_LOW = " + String (THRESH_LOW) + "; THRESH_HIGH = " + String (THRESH_HIGH);
+        html += "<br/><a href='/reset'>Reset</a><br/><a href='/update'>Update</a></html>";
+        DO_SEND (200, "text/html", html);
+    });
+
+    server.on ("/reset", [] (REQUEST_ARG) {
+        DO_SEND (200, "text/html", "<html><head><meta http-equiv=\"Refresh\" content=\"0; url='/'\"/></head></html>");
+        ESP.restart();
+    });
+
+    ElegantOTA.begin (&server);  // Start ElegantOTA
+    server.begin();
+    Serial.println ("HTTP server started");
 
     if (!lis3mdl.begin_I2C()) {  // hardware I2C mode
         Serial.println ("Failed to find LIS3MDL chip");
@@ -157,4 +180,9 @@ void setup() {
     Serial.println ("tasks created");
 }
 
-void loop() {}
+void loop () {
+#if !(ELEGANTOTA_USE_ASYNC_WEBSERVER == 1)
+    server.handleClient();
+#endif
+    ElegantOTA.loop();
+}
